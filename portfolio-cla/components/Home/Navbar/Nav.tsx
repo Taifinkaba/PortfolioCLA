53fa2d5e9917113f550790d22431f46f24baf1b3
Taifinkaba <taifinkabad@gmail.com>
2025-05-08 23:43:17 -0400

    navbar complete

--- portfolio-cla/components/Home/Navbar/Nav.tsx
+++ portfolio-cla/components/Home/Navbar/Nav.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { navLinks } from '../../../constant/constant'
 
 const Navbar = () => {
+  // color scheme changes on scroll
+  const [navBg, setNavBg]= useState(false);
+
+  useEffect(()=>{
+    const handler = () => {
+      if(window.scrollY >= 90){
+        setNavBg(true);
+      }
+      if(window.scrollY < 90){
+        setNavBg(false);
+      }
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler)
+    };
+
+  }, [])
+
+
+
   return (
-    <div className='fixed h-[12vh] z-[10] bg-blue-950 w-full'>
+    <div 
+    className={`fixed 
+  ${navBg ? "bg-[#240b39]" : "bg-blue-950"} 
+  h-[12vh] z-[10] w-full transition-all duration-200`}
+    
+    >
         <div className='flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto'>
             {/* Logo */}
-            <Image src="/images/HeadshotZoomIn.png" alt="LOGO" width={100} height={100} className='m1-[-1.5rem] sm:m1-0' />
+            <Image src="/images/HeadshotZoomIn.png" alt="LOGO" width={100} height={100} className='rounded-b-lg ml-[-1.5rem] sm:ml-0' />
 
             {/* Navbar Links */}
             <div className='flex items-center space-x-10'>
+              <div className='hidden lg:flex items-center space-x-8'>
+
+              {navLinks.map((navlink)=> {
+                return <Link key={navlink.id} href={navlink.url}>
+                  <p className='nav__link'>{navlink.label}</p>
+                </Link>
+              })
+              }
+              </div>
+
 
             </div>
 
