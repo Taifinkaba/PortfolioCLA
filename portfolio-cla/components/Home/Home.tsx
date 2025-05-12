c91d8a86e6a15c9cc970130fa2b02942b7a36d67
Taifinkaba <taifinkabad@gmail.com>
2025-05-12 02:35:38 -0400

    blog section almost complete,  replace  fluff data with my data and then make sure read more leads  to blog post link

--- portfolio-cla/components/Home/Home.tsx
+++ portfolio-cla/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import About from './About/About';
 import Services from './Services/Services';
 import Project from './Project/Project';
 import Skills from './Skills/Skills';
+import Blog from './Blog/Blog';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Services />
       <Project />
       <Skills />
+      <Blog />
     </div>
   )
 }
